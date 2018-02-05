6071cd35ab66281e1d41ed169438569121eb008a
Martin Necas <necas.marty@gmail.com>
2018-02-05 14:05:06 +0100

    corret paths to views

--- HelpingHand/HelpingHand/urls.py
+++ HelpingHand/HelpingHand/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'api/', include("MainApp.api.urls")),
-    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'admin/', admin.site.urls),
+    url(r'^api', include("MainApp.api.urls")),
+    url(r'^api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin', admin.site.urls),
     url(r'', include("MainApp.urls")),
 ]
