601e6fad10edb622518da3cc9b281c4618d393f6
Martin Necas <necas.marty@gmail.com>
2018-02-09 22:27:40 +0100

    save data from android api

--- HelpingHand/MainApp/api/urls.py
+++ HelpingHand/MainApp/api/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     url('lightInfo', views.LightInfo, name="LightInfo"),
     url('turnLight', views.turnLight, name="turnLight"),
     url('userInfo', views.UserInfo, name="UserInfo"),
+    url('deviceSave', views.deviceSave, name="UserInfo"),
+    url('configSave', views.configSave, name="UserInfo"),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- HelpingHand/MainApp/api/views.py
+++ HelpingHand/MainApp/api/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.utils import json
 
-from MainApp.api.serializers import UserSerializer, DeviceSerializer
-from MainApp.models import User, Device,Configuration,Day
+from MainApp.api.serializers import UserSerializer
+from MainApp.models import User, Device, Configuration
 
 
 @csrf_exempt
@@ -49,26 +49,60 @@ def LightInfo(request):
     if request.method == "POST":
         device_info = json.loads(request.body.decode("utf-8"))
         try:
-            Device.objects.get_or_create(ip=device_info["ip"], user=User.objects.filter(username=device_info["username"]).first())
+            Device.objects.get_or_create(ip=device_info["ip"],
+                                         user=User.objects.filter(username=device_info["username"]).first())
         except Exception as exp:
             print(exp)
         return HttpResponse("done")
 
+
 @csrf_exempt
 def UserInfo(request):
     if request.method == "GET":
-        users=User.objects.all()
-        serializer = UserSerializer(users,many=True)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return HttpResponse(json.dumps(serializer.data))
     if request.method == "POST":
         try:
-            user=User.objects.filter(username=request.POST.get("username","")).first()
+            user = User.objects.filter(username=request.POST.get("username", "")).first()
             user_serializer = UserSerializer(user)
             return HttpResponse(json.dumps(user_serializer.data))
         except Exception as exp:
             print(exp)
             return HttpResponse("error")
 
+
 def turnLight(request):
     os.system("python3 /home/mnecas/Desktop/Projects/HelpingHand/HelpingHand_server/socket_server/client.py")
     return HttpResponse("test")
+
+
+@csrf_exempt
+def deviceSave(request):
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        device_id= request.POST.get("device_id","")
+        device_name= request.POST.get("light_name","")
+        Device.objects.filter(id=device_id).update(name=device_name)
+        return HttpResponse("succes")
+
+
+@csrf_exempt
+def configSave(request):
+    if request.method == "POST":
+        print(request.POST)
+        days = []
+        for i in range(7):
+            days.append(request.POST.get("checkBoxes" + str(i), ""))
+        state = request.POST.get("state", "")
+        config_name = request.POST.get("config_name", "")
+        hours = request.POST.get("hours", "")
+        minutes = request.POST.get("minutes", "")
+        username = request.POST.get("username", "")
+        device_name = request.POST.get("device_name", "")
+        device_id = request.POST.get("device_id", "")
+        config_id = request.POST.get("config_id", "")
+        Configuration.objects.filter(id=int(config_id),
+            device=Device.objects.filter(id=int(device_id)).first()).update(hours=hours,minutes=minutes,name=config_name,state=state)
+        return HttpResponse("succes")
+
